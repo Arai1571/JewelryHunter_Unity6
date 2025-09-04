6def959abf6669430c8b69a0dde55298242dd400
runaarai <luna.alay1006@gmail.com>
2025-09-04 15:13:20 +0900

    強制スクロール「Time.deltaTime」追加

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,9 +17,6 @@ public class CameraController : MonoBehaviour
     public float scrollSpeedX = 0.5f;
     public bool isScrollY; //縦方向に強制スクロールするかのフラグ
     public float scrollSpeedY= 0.5f;
-    
-
-
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -36,7 +33,15 @@ public class CameraController : MonoBehaviour
         //一旦プレイヤーのx座標、y座標の位置を変数に取得
         x = player.transform.position.x;
         y = player.transform.position.y;
-
+        
+         //もしも横の強制スクロールフラグが立っていたら
+        if (isScrollX)
+        {
+            //前の座標に変数分だけ加算した座標
+            x = transform.position.x + (scrollSpeedX*Time.deltaTime);
+        }
+        
+        //もしも左右の限界までプレイヤーが移動したら
         if (x < leftLimit)
         {
             x = leftLimit;
@@ -45,6 +50,15 @@ public class CameraController : MonoBehaviour
         {
             x = rightLimit;
         }
+            
+        //もしも縦の強制スクロールフラグが立っていたら
+        if (isScrollY)
+        {
+            //前の座標に変数分だけ加算した座標
+            y = transform.position.y + (scrollSpeedY*Time.deltaTime);
+        }
+
+        //もしも上下の限界までプレイヤーが移動したら
         if (x < bottomLimit)
         {
             y = bottomLimit;
